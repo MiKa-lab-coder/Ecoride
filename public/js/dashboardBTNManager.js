ddb57fe25fc3e034e6355bf8a9d56cc63420284b
mika <wursthorn.mika@gmail.com>
2025-08-31 12:47:38 +0200

    fix js: edit dashboardBTNManager.js

--- public/js/dashboardBTNManager.js
+++ public/js/dashboardBTNManager.js
@@ -1,17 +1,20 @@
 // Sélection des boutons d'affichage
 const changePicDisplayBtn = document.getElementById('edit-pic-btn');
 const editProfileDisplayBtn = document.getElementById('edit-profile-btn');
-const addCarsDisplayBtn = document.getElementById('edit-car-btn');
+const addCarsDisplayBtn = document.getElementById('add-car-btn');
+const addTripDisplayBtn = document.getElementById('add-trips-btn');
 
 // Sélection des boutons d'annulation
 const cancelPicBtn = document.getElementById('cancel-pic-btn');
 const cancelEditBtn = document.getElementById('cancel-edit-btn');
-const cancelAddCarBtn = document.getElementById('cancel-car-btn');
+const cancelAddCarBtn = document.getElementById('cancel-add-car-btn');
+const cancelAddTripBtn = document.getElementById('cancel-trip-btn');
 
 // Sélection des blocs à afficher
 const changePicForm = document.querySelector('.change-pic-form');
 const editProfileForm = document.querySelector('.edit-profile');
 const addCarForm = document.querySelector('.add-car-form');
+const addTripForm = document.querySelector('.add-trip-form');
 
 // Fonction pour attacher un écouteur de bascule
 const setupToggleListener = (showBtn, cancelBtn, container) => {
@@ -37,6 +40,7 @@ export function initDashboardButtons() {
     setupToggleListener(changePicDisplayBtn, cancelPicBtn, changePicForm);
     setupToggleListener(editProfileDisplayBtn, cancelEditBtn, editProfileForm);
     setupToggleListener(addCarsDisplayBtn, cancelAddCarBtn, addCarForm);
+    setupToggleListener(addTripDisplayBtn, cancelAddTripBtn, addTripForm);
 }
 
 
