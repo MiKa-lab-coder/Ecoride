fcbca15b85698c88c56ea1cb2b4dd886c361efc8
mika <wursthorn.mika@gmail.com>
2025-09-03 08:34:33 +0200

    feat : create BaseModel.php

---
+++ src/Models/BaseModel.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models;
+use App\Models\Database\Database;
+use PDO;
+
+abstract class BaseModel
+{
+    /**
+     * @var PDO l'instance de connection a la bdd
+     */
+    protected PDO $db;
+
+    /**
+     * @var string le nom de table associé au model
+     */
+    protected string $table;
+    public function __construct(?PDO $db = null)
+    {
+        $this->db = $db ?? Database::getInstance();
+    }
+}
