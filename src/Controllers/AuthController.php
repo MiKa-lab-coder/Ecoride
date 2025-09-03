1669069685b9f32094707265ad850ea02efe4f89
mika <wursthorn.mika@gmail.com>
2025-09-03 08:37:15 +0200

    feat : create Validator.php

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use App\Services\Validator;
+use App\Services\TokenManager;
+use DateTime;
+use PDOException;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/**
+ * gere le controleur d'inscription
+ */
+class AuthController
+{
+    public function registration(): void
+    {
+        // Initialisation du logger
+        $logger = new Logger('register_logger');
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/app.log', 100));
+
+        header('Content-Type: application/json');
+
+        // Vérification que la requête est bien en POST
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405); // Méthode non autorisée
+            echo json_encode(['success' => false, 'message' => 'Méthode non autorisée.']);
+            exit;
+        }
+        // Récupération des données et du fichier uploadé
+        $data = $_POST;
+        $photo = $_FILES['photo'] ?? null;
+
+        // Validator des données
+        $validate = new Validator();
+        $errors = [];
+
+        // Échappement des données pour éviter les failles XSS
+        $name = htmlspecialchars($data['name'] ?? null);
+        $firstname = htmlspecialchars($data['firstname'] ?? null);
+        $birthdate = htmlspecialchars($data['birthdate'] ?? null);
+        $username = htmlspecialchars($data['username'] ?? null);
+        $email = htmlspecialchars($data['email'] ?? null);
+        $password = $data['password'] ?? null;
+        $confirmPassword = $data['confirmPassword'] ?? null;
+
+        // Validator des champs
+        if ($validate->validateName($name)) {
+            $errors['name'] = 'Nom invalide. Utilisez uniquement des lettres et espaces, entre 2 et 50 caractères.';
+        }
+        if ($validate->validateFirstname($firstname)) {
+            $errors['firstname'] = 'Prénom invalide. Utilisez uniquement des lettres et espaces,
+             entre 2 et 50 caractères.';
+        }
+        if (!$validate->validateBirthdate($birthdate)) {
+            $errors['birthdate'] = 'Date de naissance invalide. Utilisez le format JJ-MM-AAAA.';
+        } elseif (!$validate->validateAge($birthdate)) {
+            $errors['birthdate'] = 'Vous devez avoir au moins 18 ans pour vous inscrire.';
+        }
+        if (!$validate->validateUsername($username)) {
+            $errors['username'] = 'Nom d\'utilisateur invalide. Utilisez uniquement des caractères alphanumériques,
+             entre 3 et 20 caractères.';
+        }
+        if (!$validate->validateEmail($email)) {
+            $errors['email'] = 'Email invalide.';
+        }
+        if (!$validate->validatePassword($password)) {
+            $errors['password'] = 'Mot de passe invalide. Il doit contenir au moins 8 caractères, une majuscule,
+             une minuscule, un chiffre et un caractère spécial.';
+        } elseif ($password !== $confirmPassword) {
+            $errors['confirmPassword'] = 'Les mots de passe ne correspondent pas.';
+        }
+        if ($photo) {
+            $photoErrors = $validate->validatePhoto($photo);
+            $errors = array_merge($errors, $photoErrors);
+        }
+
+        // Si des erreurs de validation existent, on les retourne
+        if (!empty($errors)) {
+            http_response_code(400); // Requête incorrecte
+            echo json_encode(['success' => false, 'errors' => $errors]);
+            //message générique
+            exit;
+        }
+
+        //si ok, traitement de l'inscription
+        try {
+            $photo_path = 'uploads/default.png'; // chemin d'une image par défaut
+            if (isset($_FILES['photo']) && ($_FILES['photo']['error'] === UPLOAD_ERR_OK)) {
+                $uploadDir = __DIR__ . '/../../public/uploads/';
+                if (!is_dir($uploadDir)) {
+                    mkdir($uploadDir, 0755, true);
+                }
+                $photoName = uniqid() . '_' . basename($_FILES['photo']['name']);
+                $uploadFile = $uploadDir . $photoName;
+                if (move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFile)) {
+                    $photo_path = '/uploads/' . $photoName;
+                } else {
+                    $errors[] = 'Échec du téléchargement de la photo de profil.';
+                    $logger->error('Échec du téléchargement de la photo de profil.');
+                }
+            }
+
+            if (!empty($errors)) {
+                http_response_code(500); // Erreur serveur
+                echo json_encode(['success' => false, 'errors' => $errors]);
+                //message générique
+                exit;
+            }
+
+            // Création de l'utilisateur
+            $birthdateFormatted = DateTime::createFromFormat('d-m-Y', $birthdate);
+            $user = new User(
+                $name,
+                $firstname,
+                $birthdateFormatted,
+                $username,
+                $email,
+                $password,
+                $photo_path, 20,
+                0,
+                'active',
+                3
+            );
+            // Enregistrement de l'utilisateur en base de données
+            if ($user->save()) {
+                http_response_code(201); // Créé
+                echo json_encode(['success' => true, 'message' => 'Inscription réussie.']);
+                exit;
+            } else {
+                http_response_code(500); // Erreur serveur
+                echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'inscription.']);
+                exit;
+            }
+        } catch (PDOException $e) {
+            $logger->error('Erreur PDO: ' . $e->getMessage());
+            http_response_code(500); // Erreur serveur
+            echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'inscription.']);
+            exit;
+        } catch (\Exception $e) {
+            $logger->error('Erreur inscription: ' . $e->getMessage());
+            http_response_code(500); // Erreur serveur
+            echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'inscription.']);
+            //message générique pour ne pas divulguer d'infos
+            exit;
+        }
+        // fin de l'inscription
+    }
+
+    public function login(): void
+
+    {
+        $logger = new Logger('auth_logger');
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/app.log', 100));
+
+        header('Content-Type: application/json');
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405); // Méthode non autorisée
+            echo json_encode(['success' => false, 'message' => 'Méthode non autorisée.']);
+            exit;
+        }
+
+        // Récupération des données (username et password)
+        $data = json_decode(file_get_contents('php://input'), true);
+        // Échappement des données pour éviter les failles XSS
+        $username = htmlspecialchars($data['username'] ?? '');
+        $password = $data['password'] ?? '';
+
+        // Verification des champs
+        if (empty($username) || empty($password)) {
+            http_response_code(400); // Requête incorrecte
+            echo json_encode(['success' => false, 'message' => 'Nom d\'utilisateur et mot de passe sont requis.']);
+            exit;
+        }
+        // Authentification
+        try {
+            $user = User::find($username);
+            if ($user && password_verify($password, $user->getPassword())) {
+                // Authentification réussie on log l'info
+                $logger->info("Authentification réussie pour l'utilisateur: $username");
+
+                // on protège la route avec JWT
+                $tokenManager = new TokenManager();
+                $sequence = [
+                    'id' => $user->getIdUser(),
+                    'username' => $user->getUsername(),
+                    // on recupère le role de l'utilisateur pour contrôler l'accès aux ressources
+                    'role' => $user->getRoleId()
+                ];
+
+                // on génère le token
+                $jwt = $tokenManager->generateToken($sequence);
+                http_response_code(200);
+                echo json_encode(['success' => true, 'message' => 'Authentification réussie.', 'token' => $jwt]);
+                exit;
+                // redirect vers la page d'accueil ou tableau de bord gerer par le front
+
+            } else {
+                // Échec de l'authentification
+                $logger->warning("Échec de l'authentification pour l'utilisateur: $username");
+                http_response_code(401); // Non autorisé
+                echo json_encode(['success' => false, 'message' => 'Nom d\'utilisateur ou mot de passe incorrect.']);
+                exit;
+            }
+        } catch (PDOException $e) {
+            $logger->error('Erreur PDO: ' . $e->getMessage());
+            http_response_code(500); // Erreur serveur
+            echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'authentification.']);
+            exit;
+        } catch (\Exception $e) {
+            $logger->error('Erreur d\'authentification: ' . $e->getMessage());
+            http_response_code(500); // Erreur serveur
+            echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'authentification.']);
+            //message générique pour ne pas divulguer d'infos
+            exit;
+        }
+        // fin de l'authentification
+    }
+
+    public function logout(): void
+    {
+        header('Content-Type: application/json');
+        // Pour une API RESTful, le logout côté serveur est inutile car javascript(client) va detruire le token.
+
+        http_response_code(200);// OK
+        echo json_encode(['success' => true, 'message' => 'Déconnexion réussiee.']);
+        exit;
+    }
+}
