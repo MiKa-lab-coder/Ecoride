66c4751e037813598fd61f96c40aeaf2ac2e7eb5
mika <wursthorn.mika@gmail.com>
2025-09-03 10:13:59 +0200

    fix: TokenValidator.php

--- src/Services/TokenValidator.php
+++ src/Services/TokenValidator.php
@@ -16,6 +16,7 @@ class TokenValidator
 
     /**
      * Réceptionne, clean et valide un token JWT.
+     * @throws Exception
      */
 
     public function validateToken(string $token): object
@@ -23,25 +24,19 @@ class TokenValidator
         //Verification du Jwt dans le header Authorization
         $token = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
         if (!$token) {
-            http_response_code(401); // Non autorisé
-            echo json_encode(['success' => false, 'message' => 'Token manquant.']);
-            exit;
+            throw new Exception("Token manquant.", 401);// Non autorisé
         }
         // Clean du préfixe "Bearer "
         if (str_starts_with($token, 'Bearer ')) {
             $token = substr($token, 7);
         } else {
-            http_response_code(400); // Requête incorrecte
-            echo json_encode(['success' => false, 'message' => 'Format invalide.']);
-            exit;
+            throw new Exception("Format de token invalide.", 400); // Mauvaise requête
         }
         // Décodage et validation du token
         try {
             return $this->tokenManager->decode($token);
         } catch (Exception $e) {
-            http_response_code(401); // Non autorisé
-            echo json_encode(['success' => false, 'message' => 'Token invalide ou expiré.']);
-            exit;
+            throw new Exception("Token invalide.", 401);
         }
     }
 }
