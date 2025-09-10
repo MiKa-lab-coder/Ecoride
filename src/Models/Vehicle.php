6c33a263525bb467a9a10b754aad77b8804632f2
mika <wursthorn.mika@gmail.com>
2025-09-10 16:18:57 +0200

    fix : change attribute name for SQL correspondence

--- src/Models/Vehicle.php
+++ src/Models/Vehicle.php
@@ -11,41 +11,39 @@ use Monolog\Handler\StreamHandler;
 
 class Vehicle extends BaseModel
 {
-
     /**
      * @var string le nom de table associé au modèle
      */
-    protected string $table = 'vehicles';
+    protected string $table = 'VEHICLES';
 
     // Attributs
 
-    private ?int $id_car = null;
-    private DateTime $date_first_service;
-    private string $registration_number;
-    private string $energy;
+    private ?int $vehicle_id = null;
+    private DateTime $first_service;
+    private string $registration_number;// format: AB-123-XY
+    private string $energy_type;
     private string $brand;
     private string $model;
-    private string $color;
-    private int $seat_capacity;
-    private int $owner_id;
+    private ?string $color;
+    private int $seating_capacity;
+    private int $user_id;
 
 
     // Getters et Setters
-    public function getIdCar(): ?int
+    public function getVehicleId(): ?int
     {
-        return $this->id_car;
+        return $this->vehicle_id;
     }
 
-    public function setIdCar(int $id_car): void
+    public function setVehicleId(int $vehicle_id): void
     {
-        $this->id_car = $id_car;
+        $this->vehicle_id = $vehicle_id;
     }
 
-    public function getDateFirstService(): DateTime
+    public function getFirstService(): DateTime
     {
-        return $this->date_first_service;
+        return $this->first_service;
     }
-
 
     public function getRegistrationNumber(): string
     {
@@ -53,9 +51,9 @@ class Vehicle extends BaseModel
     }
 
 
-    public function getEnergy(): string
+    public function getEnergyType(): string
     {
-        return $this->energy;
+        return $this->energy_type;
     }
 
 
@@ -71,44 +69,80 @@ class Vehicle extends BaseModel
     }
 
 
-    public function getColor(): string
+    public function getColor(): ?string
     {
         return $this->color;
     }
 
 
-    public function getSeatCapacity(): int
+    public function getSeatingCapacity(): int
     {
-        return $this->seat_capacity;
+        return $this->seating_capacity;
     }
 
 
-    public function getOwnerId(): int
+    public function getUserId(): int
     {
-        return $this->owner_id;
+        return $this->user_id;
+    }
+
+    // Setters pour les propriétés
+    public function setFirstService(DateTime $first_service): void
+    {
+        $this->first_service = $first_service;
+    }
+
+    public function setRegistrationNumber(string $registration_number): void
+    {
+        $this->registration_number = $registration_number;
+    }
+
+    public function setEnergyType(string $energy_type): void
+    {
+        $this->energy_type = $energy_type;
+    }
+
+    public function setBrand(string $brand): void
+    {
+        $this->brand = $brand;
+    }
+
+    public function setModel(string $model): void
+    {
+        $this->model = $model;
+    }
+
+    public function setColor(?string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function setSeatingCapacity(int $seating_capacity): void
+    {
+        $this->seating_capacity = $seating_capacity;
+    }
+
+    public function setUserId(int $user_id): void
+    {
+        $this->user_id = $user_id;
     }
 
 
     // Constructeur
-    public function __construct(string $brand, string $model, string $registration_number, int $seat_capacity,
-                                string $color, string $energy, DateTime $date_first_service, int $owner_id, ?int $id_car = null)
+    public function __construct(string $brand, string $model, string $registration_number, int $seating_capacity,
+                                ?string $color, string $energy_type, DateTime $first_service, int $user_id,
+                                ?int $vehicle_id = null)
     {
         parent::__construct();
-        /**
-         * Initialisation des attributs
-         * On n'utilise pas les setters dans le constructeur, car on ne modifie pas un objet voiture
-         * Soit on le crée, soit on le détruit (modification interdite en France pour les véhicules)
-         */
         $this->brand = $brand;
         $this->model = $model;
         $this->registration_number = $registration_number;
-        $this->seat_capacity = $seat_capacity;
+        $this->seating_capacity = $seating_capacity;
         $this->color = $color;
-        $this->energy = $energy;
-        $this->date_first_service = $date_first_service;
-        $this->owner_id = $owner_id;
-        $this->id_car = $id_car;
-
+        $this->energy_type = $energy_type;
+        $this->first_service = $first_service;
+        $this->user_id = $user_id;
+        $this->vehicle_id = $vehicle_id;
     }
 
     // Méthodes
@@ -119,76 +153,89 @@ class Vehicle extends BaseModel
             $data['brand'],
             $data['model'],
             $data['registration_number'],
-            $data['seat_capacity'],
+            $data['seating_capacity'],
             $data['color'],
-            $data['energy'],
-            new DateTime($data['date_first_service']),
-            $data['owner_id'],
-            $data['id_car']
+            $data['energy_type'],
+            new DateTime($data['first_service']),
+            $data['user_id'],
+            $data['vehicle_id']
         );
         return $vehicle;
     }
 
     // Méthode pour enregistrer une nouvelle voiture dans la base de données
-    public static function save(Vehicle $vehicle): bool
+    public function save(): bool
     {
         $db = Database::getInstance();
         $logger = new Logger('vehicle_logger');
         $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
         try {
-            $stmt = $db->prepare("INSERT INTO vehicles (date_first_service, registration_number, energy, brand, model, color, seat_capacity, owner_id) 
-                                  VALUES (:date_first_service, :registration_number, :energy, :brand, :model, :color, :seat_capacity, :owner_id)");
-            $stmt->bindValue(':date_first_service', $vehicle->getDateFirstService()->format('Y-m-d'));
-            $stmt->bindValue(':registration_number', $vehicle->getRegistrationNumber());
-            $stmt->bindValue(':energy', $vehicle->getEnergy());
-            $stmt->bindValue(':brand', $vehicle->getBrand());
-            $stmt->bindValue(':model', $vehicle->getModel());
-            $stmt->bindValue(':color', $vehicle->getColor());
-            $stmt->bindValue(':seat_capacity', $vehicle->getSeatCapacity(), PDO::PARAM_INT);
-            $stmt->bindValue(':owner_id', $vehicle->getOwnerId(), PDO::PARAM_INT);
+            if ($this->vehicle_id) {
+                // Mise à jour
+                $sql = "UPDATE VEHICLES SET first_service = :first_service, registration_number = :registration_number,
+                    energy_type = :energy_type, brand = :brand, model = :model, color = :color,
+                    seating_capacity = :seating_capacity, user_id = :user_id WHERE vehicle_id = :vehicle_id";
+                $stmt = $db->prepare($sql);
+                $stmt->bindValue(':vehicle_id', $this->vehicle_id, PDO::PARAM_INT);
+            } else {
+                // Insertion
+                $sql = "INSERT INTO VEHICLES (first_service, registration_number, energy_type, brand, model, color,
+                      seating_capacity, user_id) 
+                                  VALUES (:first_service, :registration_number, :energy_type, :brand, :model, :color,
+                                          :seating_capacity, :user_id)";
+                $stmt = $db->prepare($sql);
+            }
+
+            $stmt->bindValue(':first_service', $this->first_service->format('Y-m-d'));
+            $stmt->bindValue(':registration_number', $this->registration_number);
+            $stmt->bindValue(':energy_type', $this->energy_type);
+            $stmt->bindValue(':brand', $this->brand);
+            $stmt->bindValue(':model', $this->model);
+            $stmt->bindValue(':color', $this->color);
+            $stmt->bindValue(':seating_capacity', $this->seating_capacity, PDO::PARAM_INT);
+            $stmt->bindValue(':user_id', $this->user_id, PDO::PARAM_INT);
 
             if ($stmt->execute()) {
-                // Récupérer l'ID et le définir dans l'objet
-                $vehicle->setIdCar((int)$db->lastInsertId());
+                if (!$this->vehicle_id) {
+                    $this->vehicle_id = (int)$db->lastInsertId();
+                }
                 return true;
             } else {
-                $logger->error('Échec de l\'insertion de la voiture dans la base de données.');
+                $logger->error('Échec de l\'enregistrement du véhicule dans la base de données.');
                 return false;
             }
         } catch (PDOException $e) {
-            // Gérer les erreurs de connexion ou d'exécution
             $logger->error('Erreur PDO', ['exception' => $e]);
             return false;
         }
     }
 
-    // Méthode pour supprimer ma voiture (sélection par plaque d'immatriculation)
-    public static function delete(string $registration_number): bool
+    // Méthode pour supprimer un véhicule
+    public static function delete(int $vehicle_id): bool
     {
         $db = Database::getInstance();
         $logger = new Logger('vehicle_logger');
         $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
         try {
-            $stmt = $db->prepare("DELETE FROM vehicles WHERE registration_number = :registration_number");
-            $stmt->bindValue(':registration_number', $registration_number);
+            $stmt = $db->prepare("DELETE FROM VEHICLES WHERE vehicle_id = :vehicle_id");
+            $stmt->bindValue(':vehicle_id', $vehicle_id, PDO::PARAM_INT);
             return $stmt->execute();
         } catch (PDOException $e) {
-            // Gérer les erreurs de connexion ou d'exécution
             $logger->error('Erreur PDO', ['exception' => $e]);
             return false;
         }
     }
 
-    // Méthode pour recuperer les voitures (par utilisateur)
-    public static function getVehiclesByOwnerId(int $owner_id): array
+    // Méthode pour recuperer les véhicules par utilisateur
+    public static function getVehiclesByUserId(int $user_id): array
     {
         $db = Database::getInstance();
         $logger = new Logger('vehicle_logger');
         $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
         $vehicles = [];
         try {
-            $stmt = $db->prepare("SELECT * FROM vehicles WHERE owner_id = :owner_id");
-            $stmt->bindValue(':owner_id', $owner_id, PDO::PARAM_INT);
+            $stmt = $db->prepare("SELECT * FROM VEHICLES WHERE user_id = :user_id");
+            $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
             $stmt->execute();
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -196,12 +243,34 @@ class Vehicle extends BaseModel
                 $vehicles[] = self::hydrate($data);
             }
         } catch (PDOException $e) {
-            // Gérer les erreurs de connexion ou d'exécution
             $logger->error('Erreur PDO', ['exception' => $e]);
             return [];
         }
         return $vehicles;
     }
+
+    // trouver un véhicule par son ID
+    public static function findById(int $vehicle_id): ?Vehicle
+    {
+        $db = Database::getInstance();
+        $logger = new Logger('vehicle_logger');
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
+        try {
+            $stmt = $db->prepare("SELECT * FROM VEHICLES WHERE vehicle_id = :vehicle_id");
+            $stmt->bindValue(':vehicle_id', $vehicle_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($data) {
+                return self::hydrate($data);
+            } else {
+                return null;
+            }
+        } catch (PDOException $e) {
+            $logger->error('Erreur PDO', ['exception' => $e]);
+            return null;
+        }
+    }
+
     // trouver un véhicule par son numéro d'immatriculation
     public static function findByRegistration(string $registration_number): ?Vehicle
     {
@@ -209,7 +278,7 @@ class Vehicle extends BaseModel
         $logger = new Logger('vehicle_logger');
         $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
         try {
-            $stmt = $db->prepare("SELECT * FROM vehicles WHERE registration_number = :registration_number");
+            $stmt = $db->prepare("SELECT * FROM VEHICLES WHERE registration_number = :registration_number");
             $stmt->bindValue(':registration_number', $registration_number);
             $stmt->execute();
             $data = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -219,33 +288,30 @@ class Vehicle extends BaseModel
                 return null;
             }
         } catch (PDOException $e) {
-            // Gérer les erreurs de connexion ou d'exécution
             $logger->error('Erreur PDO', ['exception' => $e]);
             return null;
         }
     }
-    // Methode pour trouver un véhicule par son énergie pour possibilité de filtre (thermique, électrique, hybride)
-    public static function findByEnergy(string $energy): array
+
+    // Methode pour trouver un véhicule par son énergie pour possibilité de filtre (combustion, électrique, hybride)
+    public static function findByEnergy(string $energy_type): array
     {
         $db = Database::getInstance();
         $logger = new Logger('vehicle_logger');
         $logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/vehicle.log', 100));
         $vehicles = [];
         try {
-            $stmt = $db->prepare("SELECT * FROM vehicles WHERE energy = :energy");
-            $stmt->bindValue(':energy', $energy);
+            $stmt = $db->prepare("SELECT * FROM VEHICLES WHERE energy_type = :energy_type");
+            $stmt->bindValue(':energy_type', $energy_type);
             $stmt->execute();
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             foreach ($results as $data) {
                 $vehicles[] = self::hydrate($data);
             }
         } catch (PDOException $e) {
-            // Gérer les erreurs de connexion ou d'exécution
             $logger->error('Erreur PDO', ['exception' => $e]);
             return [];
         }
         return $vehicles;
     }
-
 }
-
