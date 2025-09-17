e4a0c9cb7c395592e9c78eec8724eea9eb5c93b1
mika <wursthorn.mika@gmail.com>
2025-09-17 21:19:31 +0200

    fix ascync after dockerbuild

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,29 @@
-import{loadHeadFoot} from "./addHeaderFooter.js";
-import {setBurger} from "./burger.js";
-import {setFilterBtn} from "./filterBtn.js";
-import {viewDetails} from "./moreDetails.js";
-import {initDashboardButtons} from "./dashboardBTNManager.js";
+// Importation des modules nécessaires
+import { loadHeadFoot } from "./addHeaderFooter.js";
+import { setBurger } from "./burger.js";
+import { setFilterBtn } from "./filterBtn.js";
+import { viewDetails } from "./moreDetails.js";
+import { initDashboardButtons } from "./dashboardBTNManager.js";
 import { initSectionMenu } from "./sectionMenuManager.js";
 import { displaySidebarByRoles } from "./displaySidebarByRoles.js";
 
-//appel de la fonction pour charger l'en-tête et le pied de page
-document.addEventListener('DOMContentLoaded',async () => {
-    await loadHeadFoot('/Ecoride/public/html/includes/header.html', 'header-placeholder');
-    await loadHeadFoot('/Ecoride/public/html/includes/footer.html', 'footer-placeholder');
+// Fonction principale pour initialiser l'application
+async function initApp() {
+    // Charger l'en-tête et le pied de page
+    await loadHeadFoot('/html/includes/header.html', 'header-placeholder');
+    await loadHeadFoot('/html/includes/footer.html', 'footer-placeholder');
 
-    //appel de la fonction pour gérer le menu burger
+    // Exécuter tous les scripts qui ont besoin de ces éléments HTML
     setBurger();
-
-    //appel de la fonction pour gérer le bouton de filtre
     setFilterBtn();
-
-    //appel de la fonction pour gérer le bouton de détails
     viewDetails();
-
-    //appel de la fonction pour gérer les boutons du dashboard
     initDashboardButtons();
-
-    //appel de la fonction pour gérer le menu de section
     initSectionMenu();
 
-    //role en dur pour test
-    const userRole = document.body.dataset.userRole || 'admin'; // Valeur par défaut 'user' si non définie
+    // Rôle en dur pour les tests
+    const userRole = document.body.dataset.userRole || 'admin';
     displaySidebarByRoles(userRole);
+}
 
-});
+// Lancer l'initialisation de l'application une fois que le DOM est chargé
+document.addEventListener('DOMContentLoaded', initApp);
