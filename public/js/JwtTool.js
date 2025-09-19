a00e12342f42f431f91b2bb171be5d6f49273084
mika <wursthorn.mika@gmail.com>
2025-09-19 16:37:56 +0200

    fix :add if

--- public/js/JwtTool.js
+++ public/js/JwtTool.js
@@ -1,6 +1,10 @@
 // Fonction utilitaire pour décoder un token JWT
 export function parseJwt(token) {
     try {
+        if (!token) {
+            console.warn('Token JWT non fourni.');
+            return null;
+        }
         const base64Url = token.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
