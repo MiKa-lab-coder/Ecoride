1ef76a57674609ae75bc8a80082a9a1e4fafa693
mika <wursthorn.mika@gmail.com>
2025-09-20 01:45:55 +0200

    feat: create logoutFetch.js

---
+++ public/js/logoutFetch.js
@@ -0,0 +1,16 @@
+// Methode pour se déconnecter et supprimer le token du localStorage
+export function logout() {
+    localStorage.removeItem('token');
+    window.location.href = '/index.html'; // Redirige vers la page d'accueil
+}
+
+// Le gestionnaire d'événements qui appelle la fonction de déconnexion
+document.addEventListener('DOMContentLoaded', () => {
+    const logoutBtn = document.getElementById('logout-button');
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', (e) => {
+            e.preventDefault(); // Empêche le comportement par défaut du lien
+            logout(); // Appelle la fonction de déconnexion
+        });
+    }
+});
